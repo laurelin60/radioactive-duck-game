a1fc922ab3ea3a49f37a28b365eb313c64daeff6
smalldrew <idk.contact1000@gmail.com>
2024-02-25 12:14:45 -0800

    refactor: delete unneeded functions

--- Assets/Scripts/DisplayScore.cs
+++ Assets/Scripts/DisplayScore.cs
@@ -5,16 +5,9 @@ using UnityEngine.UI;
 
 public class DisplayScore : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Text score;
     void Start()
     {
         score.text = GUIManager.ScoreState;     
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
