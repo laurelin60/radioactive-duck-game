205eeae3de985898752e3629160bab87db5f942d
Kevin Wu <kevinwu098@gmail.com>
2024-02-24 18:39:12 -0800

    wip: instantiation & deletion 5:26

---
+++ Assets/DuckSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpawner : MonoBehaviour
+{
+    public GameObject Duck;
+
+    public void SpawnDuck() {
+        // Instantiates duck
+    }
+}
