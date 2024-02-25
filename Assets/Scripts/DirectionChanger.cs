664d51b5d51941effb9a03621ef2c915aa4d6071
smalldrew <idk.contact1000@gmail.com>
2024-02-24 19:50:32 -0800

    feat: bouncing ducks

---
+++ Assets/Scripts/DirectionChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionChanger : MonoBehaviour
+{
+    public enum DirChanger {Horizontal, Vertical};
+    public DirChanger changer;
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if(hit.transform.tag == "Duck")
+        {
+            // create duck movement variable and assign using getcomponentof hit
+            DuckMovement duck = hit.gameObject.GetComponent<DuckMovement>();
+
+            if(changer == DirChanger.Horizontal)
+            {
+                duck.DirectionChanger(new Vector3(-1, 1, 0));
+            }
+            else if(changer == DirChanger.Vertical)
+            {
+                duck.DirectionChanger(new Vector3(1, -1, 0));
+            }
+        }
+    } 
+}
---
+++ Assets/Scripts/DuckMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckMovement : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+
+    void Start()
+    {
+        RandomDirection();
+    }
+
+    void Update()
+    {
+        //move the duck by changing the position of the transform. Times directions with speed.
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void RandomDirection()
+    {
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(.2f, 1f), 0);
+    }
+
+    public void DirectionChanger(Vector3 _dir)
+    {
+        direction = new Vector3(direction.x * _dir.x, direction.y * _dir.y, 0);
+    }
+}
